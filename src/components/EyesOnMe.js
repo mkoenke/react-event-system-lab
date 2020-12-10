55ccc7e6f9e70c7e293f0fc4a8e54193e6345291
mkoenke <mkoenke@sas.upenn.edu>
2020-12-09 19:54:17 -0500

    commit tree ce2d1fd68805a534dd8b73ea7b1b115e4c185665

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+class EyesOnMe extends React.Component {
+  handleFocus = () => console.log("Good!");
+  onBlur = () => console.log("Hey! Eyes on me!");
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
